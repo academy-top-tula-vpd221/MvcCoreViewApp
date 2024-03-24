e2b11bc6dc2edd47dcde1db7dcdb2d2014b4faa9
maxefimov <efimov.top.academy@gmail.com>
2024-03-24 17:51:37 +0300

    Добавьте файлы проекта.

---
+++ MvcCoreViewApp/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreViewApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Home page";
+            ViewBag.Hello = "Hello people!";
+            return View();
+        }
+    }
+}
